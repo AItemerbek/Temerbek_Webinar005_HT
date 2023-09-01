c7a49fe235403470b474462f25fb4c58e2eda485
alekseytemerbek <tech.temerbek@gmail.com>
2023-09-01 12:39:42 +0300

    add Home Task 27

---
+++ 230901_HT_Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNum(string message) 
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int ConvertNumberToSumDigit(int num)
+{
+    int sumDigit = 0;
+    while (num > 0)
+    {
+        sumDigit = sumDigit + num % 10;
+        num /= 10;
+    }
+    return sumDigit;
+}
+
+Console.Clear();
+
+int number = InputNum("Введите число чтобы получить сумму его цифр ");
+Console.WriteLine($"Сумма всех цыфр числа{number} равняется {ConvertNumberToSumDigit(number)}");
